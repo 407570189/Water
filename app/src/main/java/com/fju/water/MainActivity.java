5f47dffe0a663e4c264690c6010fbbdc108cc63c
miia <miia@com.tw>
2019-12-03 16:19:21 +0800

    hyw2

--- app/src/main/java/com/fju/water/MainActivity.java
+++ app/src/main/java/com/fju/water/MainActivity.java
@@ -18,16 +18,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG ="TAG" ;
+    private static final String TAG ="Main" ;
     private EditText months;
     private EditText nexts;
     boolean isNext =false;
@@ -87,6 +89,18 @@ public class MainActivity extends AppCompatActivity {
                 isNext=isChecked;
                 TextView text=findViewById(R.id.type);
                 text.setText(isNext?getText(R.string.every_other_monthly):getText(R.string.monthly));
+            }
+        });
+       Spinner cities=findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
         Button button=findViewById(R.id.button);
